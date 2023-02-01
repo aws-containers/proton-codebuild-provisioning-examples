658a02835ace69edcc604ec05b185a0891d23229
Adam Keller <adam@adamjkeller.com>
2023-02-01 09:00:52 -0800

    Testing deploy order to avoid complexity

--- cdk/environment-templates/cdk-vpc-eks-cluster/v1/infrastructure/cdk_deploy.sh
+++ cdk/environment-templates/cdk-vpc-eks-cluster/v1/infrastructure/cdk_deploy.sh
@@ -5,21 +5,24 @@
 # To solve this, we deploy the cluster-stack stack first, then we can deploy the cluster stack 
 ###
 
-VERSION=v4.30.6
-BINARY=yq_linux_amd64
-
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
-  tar xz && mv ${BINARY} /usr/bin/yq
+#VERSION=v4.30.6
+#BINARY=yq_linux_amd64
+#
+#wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
+#  tar xz && mv ${BINARY} /usr/bin/yq
 
 env_name=$(cat proton-inputs.json | jq -r '.environment.name')
-current_ns_length=$(aws proton get-environment --name $env_name | jq -r '.environment.spec' | yq '.spec.namespaces|length')
-new_ns_length=$(cat proton-inputs.json | jq -r '.environment.inputs.namespaces | length')
+echo "Deploying to ${env_name}"
+npm run cdk -- deploy --exclusively "${env_name}-cluster-stack"
+npm run cdk -- deploy --exclusively "${env_name}-cluster"
+#current_ns_length=$(aws proton get-environment --name $env_name | jq -r '.environment.spec' | yq '.spec.namespaces|length')
+#new_ns_length=$(cat proton-inputs.json | jq -r '.environment.inputs.namespaces | length')
 
-if [[ ${new_ns_length} -lt ${current_ns_length} ]]; then
-  echo -e "Current namespace count: ${current_ns_length}\nProposed namespace count: ${new_ns_length}"
-  echo "Namespaces have been removed, running cdk in a specific order to adhere to dependencies between stacks"
-  npm run cdk -- deploy --exclusively "${env_name}-cluster-stack"
-  npm run cdk -- deploy --exclusively "${env_name}-cluster"
-else
-  npm run cdk -- deploy --all
-fi
+#if [[ ${new_ns_length} -lt ${current_ns_length} ]]; then
+#  echo -e "Current namespace count: ${current_ns_length}\nProposed namespace count: ${new_ns_length}"
+#  echo "Namespaces have been removed, running cdk in a specific order to adhere to dependencies between stacks"
+#  npm run cdk -- deploy --exclusively "${env_name}-cluster-stack"
+#  npm run cdk -- deploy --exclusively "${env_name}-cluster"
+#else
+#  npm run cdk -- deploy --all
+#fi
